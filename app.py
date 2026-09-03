f5b19e4ab59d89648033882ff610881dc7fd6e76
sardaratiqrasheed-cell <sardaratiqrasheed@gmail.com>
2026-09-03 23:42:11 +0500

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 # Configure the browser tab title and favicon
 st.set_page_config(
-    page_title="Telecom Site Search",
+    page_title="4G Cell FDD",
     page_icon="📡",
     layout="wide"
 )
